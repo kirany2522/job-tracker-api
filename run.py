353549b0a0921c091a20d3542b902464f814853a
Kiran Yadav <coekiran800024@gmail.com>
2025-06-23 03:43:05 +0530

    Add jobs.db with run file changes

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
+
+from app import app, db
+
+if __name__ == '__main__':
+    import os
+    # Ensure tables are created on server startup
+    with app.app_context():
+        db.create_all()
+        print("✅ Tables created or verified.")
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 from app import app
 
 
